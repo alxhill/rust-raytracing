2afae08b114d75fae6211ac2018ad66bb28bceb2
Alexander Hill <me@alxhill.com>
2023-02-17 19:07:55 -0500

    add click to start/stop rendering

--- rt-wasm/html/index.js
+++ rt-wasm/html/index.js
@@ -1,22 +1,37 @@
 import * as wasm from "rt-wasm";
 import { memory } from "rt-wasm/rt_wasm_bg";
 
-const start = performance.now();
-let output = wasm.render();
-console.log(`rendering took ${performance.now() - start} ms`);
+let scene = wasm.JsScene.new();
 
-let pixel_start_ptr = output.pixels();
-let pixels_array = new Uint8ClampedArray(memory.buffer, pixel_start_ptr, output.width() * output.height() * 4);
+
+let pixels_array = new Uint8ClampedArray(memory.buffer, scene.pixels(), scene.width() * scene.height() * 4);
 
 let canvas = document.getElementById("raytracing");
-canvas.width = output.width();
-canvas.height = output.height();
+canvas.width = scene.width();
+canvas.height = scene.height();
 
 let ctx = canvas.getContext("2d");
 
+var render_loop = true;
+
+canvas.addEventListener("click", function (e) {
+    render_loop = !render_loop;
+    if (render_loop) {
+        drawRender();
+    }
+});
+
 const drawRender = () => {
-    console.log("drawing to screen");
-    ctx.putImageData(new ImageData(pixels_array, output.width(), output.height()), 0, 0);
+    const start = performance.now();
+    scene.render();
+    console.log(`rendering took ${performance.now() - start} ms`);
+
+    ctx.putImageData(new ImageData(pixels_array, scene.width(), scene.height()), 0, 0);
+
+    scene.move_camera(0, 0, 1.0);
+
+    if (render_loop)
+        requestAnimationFrame(drawRender);
 }
 
 drawRender();
