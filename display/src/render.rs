8b3114bfed51cd052d9cd7a539dbf746e4278b8b
Alexander Hill <me@alxhill.com>
2023-03-02 22:54:26 -0500

    change

--- display/src/render.rs
+++ display/src/render.rs
@@ -11,17 +11,17 @@ pub fn render_parallel<R: Renderable + Sync, T: RenderTarget>(
     img: &mut T,
 ) {
     let pixel_array = view_plane.pixel_array();
-    let pixel_chunks = pixel_array.chunks(view_plane.width as usize).collect::<Vec<_>>();
+    let pixel_chunks = pixel_array.chunks(1024).collect::<Vec<_>>();
 
     let output = pixel_chunks.par_iter().map(|chunk| {
         let mut output_row = Vec::new();
         for xy in *chunk {
-            output_row.push(renderer.render_pixel(&xy));
+            output_row.push(renderer.render_pixel(xy));
         }
         output_row
     }).flatten().collect::<Vec<RGBColor>>();
 
     for (i, color) in output.iter().enumerate() {
-        img.set_pixel(&ViewXY(i % view_plane.width, i / view_plane.height), &color);
+        img.set_pixel(&ViewXY(i % view_plane.width, i / view_plane.height), color);
     }
 }
